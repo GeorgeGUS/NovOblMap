ba3851584542b0e0afe086f3c60c8bb18de9b400
GeorgeGUS <georgegus1@gmail.com>
2018-12-14 13:04:45 +0300

    Wrap routes in IIFE, add comments

--- js/init.js
+++ js/init.js
@@ -9,15 +9,19 @@ const ch2mux = 'сh2mux'
 
 ymaps.ready(init)
 function init () {
-  var myMap = new ymaps.Map('map', {
-    center: [58.19460497, 32.9240815],
-    controls: [], // убрать все элементы управления
-    zoom: 7.9
-  }, {
-    avoidFractionalZoom: false, //Разрешает дробное значение зума
-  })
-  //Запретить манипуляции с картой
-  myMap.behaviors.disable(['drag', 'scrollZoom', 'dblClickZoom']);
+  var myMap = new ymaps.Map(
+    'map',
+    {
+      center: [58.19460497, 32.9240815],
+      controls: [], // убрать все элементы управления
+      zoom: 7.9
+    },
+    {
+      avoidFractionalZoom: false // Разрешает дробное значение зума
+    }
+  )
+  // Запретить манипуляции с картой
+  myMap.behaviors.disable(['drag', 'scrollZoom', 'dblClickZoom'])
 
   // Получаем данные по пунктам вещания ЦТВ
   $.getJSON(DATA_URL, function (data) {
@@ -43,78 +47,82 @@ function init () {
       myMap.geoObjects.add(myPlacemark)
     }
 
-    // Получаем объект с координатами головных цехов
-    var cehCoords = {}
-    data.forEach(pin => {
-      if (pin[ceh] === pin.name) {
-        cehCoords[pin.name] = [pin.lat, pin.len]
-      }
-    })
-
-    // Проводим прямые линии от головных цехов до их пунктов
-    data.forEach((pin, i) => {
-      var myPolyline = new ymaps.Polyline(
-        [cehCoords[pin[ceh]], [pin.lat, pin.len]],
-        {},
-        {
-          strokeWidth: 3,
-          strokeColor: '#2196f3'
+    //Рисуем маршруты (линии) от цехов до пунктов
+    ;(function () {
+      // Получаем объект с координатами головных цехов
+      var cehCoords = {}
+      data.forEach(pin => {
+        if (pin[ceh] === pin.name) {
+          cehCoords[pin.name] = [pin.lat, pin.len]
         }
-      )
-      if (i < 1) {
-        console.log(myPolyline)
-      }
-      myMap.geoObjects.add(myPolyline)
-    })
+      })
 
-    // Проводим маршруты от головных цехов до их пунктов
-    // data.forEach((pin, i) => {
-    //   var route = new ymaps.multiRouter.MultiRoute(
-    //     {
-    //       referencePoints: [cehCoords[pin[ceh]], [pin.lat, pin.len]]
-    //     },
-    //     {
-    //       wayPointVisible: false,
-    //       // Внешний вид линии активного маршрута.
-    //       routeActiveStrokeWidth: 5,
-    //       routeActiveStrokeStyle: 'solid',
-    //       routeActiveStrokeColor: '#2196f3',
-    //       // Внешний вид линий альтернативных маршрутов.
-    //       routeStrokeStyle: 'dot',
-    //       routeStrokeColor: '#0000',
-    //       routeStrokeWidth: 1
-    //     }
-    //   )
-    //   if (i < 1) {
-    //     console.log(route)
-    //   }
-    //   myMap.geoObjects.add(route)
-    // })
+      // Проводим прямые линии от головных цехов до их пунктов
+      data.forEach((pin, i) => {
+        var myPolyline = new ymaps.Polyline(
+          [cehCoords[pin[ceh]], [pin.lat, pin.len]],
+          {},
+          {
+            strokeWidth: 3,
+            strokeColor: '#2196f3'
+          }
+        )
+        if (i < 1) {
+          console.log(myPolyline)
+        }
+        myMap.geoObjects.add(myPolyline)
+      })
 
-    // Set the route.
-    // A driving route is created by default.
-    // var multiRoute = new ymaps.multiRouter.MultiRoute(
-    //   {
-    //     // Route points. Points can be set as coordinates or addresses.
-    //     referencePoints: [
-    //       'Smolenskaya metro station, Moscow',
-    //       'Arbatskaya metro station, Moscow',
-    //       [55.734876, 37.59308] // Lva Tolstogo street
-    //     ]
-    //   },
-    //   {
-    //     // |Automatically set the map viewport so that
-    //     // the entire route is visible.
-    //     boundsAutoApply: true
-    //   }
-    // )
+      // Проводим маршруты от головных цехов до их пунктов
+      // data.forEach((pin, i) => {
+      //   var route = new ymaps.multiRouter.MultiRoute(
+      //     {
+      //       referencePoints: [cehCoords[pin[ceh]], [pin.lat, pin.len]]
+      //     },
+      //     {
+      //       wayPointVisible: false,
+      //       // Внешний вид линии активного маршрута.
+      //       routeActiveStrokeWidth: 5,
+      //       routeActiveStrokeStyle: 'solid',
+      //       routeActiveStrokeColor: '#2196f3',
+      //       // Внешний вид линий альтернативных маршрутов.
+      //       routeStrokeStyle: 'dot',
+      //       routeStrokeColor: '#0000',
+      //       routeStrokeWidth: 1
+      //     }
+      //   )
+      //   if (i < 1) {
+      //     console.log(route)
+      //   }
+      //   myMap.geoObjects.add(route)
+      // })
 
-    // Add the route to the map.
-    // myMap.geoObjects.add(multiRoute)
+      // Set the route.
+      // A driving route is created by default.
+      // var multiRoute = new ymaps.multiRouter.MultiRoute(
+      //   {
+      //     // Route points. Points can be set as coordinates or addresses.
+      //     referencePoints: [
+      //       'Smolenskaya metro station, Moscow',
+      //       'Arbatskaya metro station, Moscow',
+      //       [55.734876, 37.59308] // Lva Tolstogo street
+      //     ]
+      //   },
+      //   {
+      //     // |Automatically set the map viewport so that
+      //     // the entire route is visible.
+      //     boundsAutoApply: true
+      //   }
+      // )
+
+      // Add the route to the map.
+      // myMap.geoObjects.add(multiRoute)
+    })()
   })
 
   const NOV_OBL_INDEX = 32
 
+  // Рисует Новгородскую область
   ymaps.borders.load('RU', { quality: 2 }).then(
     function (geojson) {
       console.dir(geojson.features[NOV_OBL_INDEX])
