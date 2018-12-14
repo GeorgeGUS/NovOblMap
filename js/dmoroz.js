562a8d27a4a0f8f6452646ea0a3e6f83b24425ee
GeorgeGUS <georgegus1@gmail.com>
2018-12-15 00:50:01 +0300

    Add Ded jumping in the end of the route

--- js/dmoroz.js
+++ js/dmoroz.js
@@ -10,39 +10,28 @@ export function DMoroz(routePoints, map, ymaps) {
 
     var points = [
         routePoints['Великий Новгород'],
-        {
-            type: 'viaPoint',
-            point: routePoints['Залучье'],
-        },
+        // {
+        //     type: 'viaPoint',
+        //     point: routePoints['Залучье'],
+        // },
         {
             type: 'viaPoint',
             point: routePoints['Пролетарий'],
         },
-        routePoints['Боровичи']
+        // routePoints['Боровичи']
     ]
 
     //Маршрут Деда Мороза
     ymaps.route(points).then(function(route) {
-        // Добавление маршрута на карту
-        // map.geoObjects.add(route); //Можно оставить для отладки
-        // И "Деда" туда же
         map.geoObjects.add(ded);
-        // Отправляем машинку по полученному маршруту
         ded.moveTo(route.getPaths().get(0).getSegments(), {
             speed: 4000,
             directions: 8
-        }, function (geoObject, coords, direction) { // тик движения
-            // перемещаем машинку
+        }, function (geoObject, coords, direction) {
             geoObject.geometry.setCoordinates(coords);
-            // ставим машинке правильное направление - в данном случае меняем ей текст
             geoObject.properties.set('direction', direction.t);
-
-        }, function (geoObject) { // приехали
-            // geoObject.properties.set('balloonContent', "Приехали!");
-            // geoObject.balloon.open();
-            // ded.stop()
-        });
-
-        
+        }, function (geoObject) {
+            geoObject.properties.set('direction', 'jump');
+        });        
     })
 }
