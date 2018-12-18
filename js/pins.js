acdf2fc1bea74c922b57e79695f8598a4fca8dce
GeorgeGUS <georgegus1@gmail.com>
2018-12-19 01:54:20 +0300

    Change default icons on trees for each ceh

--- js/pins.js
+++ js/pins.js
@@ -1,19 +1,30 @@
 export function drawPins (data, isActive) {
   var PinLabelClass = ymaps.templateLayoutFactory.createClass(
-    '<div class="pin $[properties.mainClass] $[properties.activeClass]">{{ properties.pinLabel }}</div>'
+    `<div class="pin $[properties.iconClass] $[properties.mainClass] $[properties.activeClass]">     
+      {{ properties.pinLabel }}
+    </div>`
   )
+  var cehIconClass = {
+    'Великий Новгород': 'pin-vn',
+    Пролетарий: 'pin-pr',
+    Боровичи: 'pin-br',
+    Залучье: 'pin-zl'
+  }
   var activeClass = isActive ? 'active' : ''
+
   for (var pin of data) {
+    var iconClass = cehIconClass[pin.ceh]
     var mainClass = pin.ceh === pin.name ? 'pin-main' : ''
     var myPlacemark = new ymaps.Placemark(
       [pin.lat, pin.len],
       {
+        iconClass: iconClass,
         mainClass: mainClass,
         activeClass: activeClass,
-        pinLabel: pin.name,
+        pinLabel: pin.name
       },
       {
-        iconLayout: PinLabelClass,
+        iconLayout: PinLabelClass
       }
     )
     myMap.geoObjects.add(myPlacemark)
